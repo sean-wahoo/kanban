9a40f6b63e1c8800253b8f408c42472cdf6a8b92
sean <sean.p.reichel@gmail.com>
2026-05-26 04:55:30 -0400

    i hate envs

--- src/env.mjs
+++ src/env.mjs
@@ -6,7 +6,7 @@ export const env = createEnv({
     DATABASE_URL: z.string(),
     TURSO_DATABASE_URL: z.string(),
     TURSO_DATABASE_TOKEN: z.string(),
-    BETTER_AUTH_URL: z.string(),
+    BETTER_AUTH_URL: z.optional(z.string()),
     BETTER_AUTH_SECRET: z.string(),
     ADMIN_EMAIL: z.string(),
     ADMIN_PASSWORD: z.string(),
@@ -17,10 +17,12 @@ export const env = createEnv({
     VERCEL_URL: z.optional(z.string()),
   },
   client: {
-    NEXT_PUBLIC_NODE_ENV: z.enum(["test", "development", "production"]),
-    NEXT_PUBLIC_PORT: z
-      .string()
-      .refine((s) => !isNaN(Number(s)) && isFinite(Number(s))),
+    NEXT_PUBLIC_NODE_ENV: z
+      .enum(["test", "development", "production"])
+      .default("development"),
+    NEXT_PUBLIC_PORT: z.optional(
+      z.string().refine((s) => !isNaN(Number(s)) && isFinite(Number(s))),
+    ),
     NEXT_PUBLIC_BASE_URL: z.optional(z.string()),
     NEXT_PUBLIC_VERCEL_URL: z.optional(z.string()),
   },
