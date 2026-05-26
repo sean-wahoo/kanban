2ea054aee206a4dd894a45c28e7f806a2057ccb9
sean <sean.p.reichel@gmail.com>
2026-05-26 04:37:12 -0400

    prisma things

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,30 +4,49 @@ import { prisma } from "@/lib/prisma";
 import { Colors } from "@/lib/utils/shared";
 
 interface DefaultStatusType {
+  id: string;
   name: string;
   color: string;
 }
 const DEFAULT_STATUSES: DefaultStatusType[] = [
-  { name: "backlog", color: Colors.blue },
-  { name: "in_progress", color: Colors.orange },
-  { name: "finished", color: Colors.orange },
+  { id: "DEFAULT_BACKLOG", name: "backlog", color: Colors.blue },
+  { id: "DEFAULT_IN_PROGRESS", name: "in_progress", color: Colors.orange },
+  { id: "DEFAULT_FINISHED", name: "finished", color: Colors.green },
 ];
 
 async function main() {
   try {
     const adminEmail = env.ADMIN_EMAIL;
     const adminPassword = env.ADMIN_PASSWORD;
-    await auth.api.signUpEmail({
-      body: {
-        email: adminEmail,
-        password: adminPassword,
-        name: "sean",
-      },
+
+    let adminUser = await prisma.user.findUnique({
+      where: { email: adminEmail },
     });
+    if (adminUser) {
+      console.log("user already exists");
+    } else {
+      await auth.api.signUpEmail({
+        body: {
+          email: adminEmail,
+          password: adminPassword,
+          name: "sean",
+        },
+      });
+
+      adminUser = await prisma.user.findUnique({
+        where: { email: adminEmail },
+      });
+
+      if (!adminUser) {
+        throw new Error("user not found");
+      }
+    }
 
     DEFAULT_STATUSES.map(async (status) => {
-      await prisma.status.create({
-        data: status,
+      await prisma.status.upsert({
+        where: { id: status.id },
+        update: {},
+        create: { ...status, userId: adminUser.id },
       });
     });
   } catch (e) {
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,19 @@
 import { env } from "@/env.mjs";
 import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 
-const url = `${env.DATABASE_URL}`;
+const adapter =
+  env.NODE_ENV !== "production"
+    ? new PrismaBetterSqlite3({ url: "file:./dev.db" })
+    : new PrismaLibSql({
+        url: env.TURSO_DATABASE_URL,
+        authToken: env.TURSO_DATABASE_TOKEN,
+      });
+
 const createPrismaClient = () =>
   new PrismaClient({
-    adapter: new PrismaBetterSqlite3({ url }),
+    adapter,
   });
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient>;
