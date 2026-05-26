73a8aeeda060aa0e9084b27e9628b3c1db1076c6
sean <sean.p.reichel@gmail.com>
2026-05-26 14:41:05 -0400

    url stuff

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,21 @@ const nextConfig: NextConfig = {
   turbopack: {
     debugIds: true,
   },
+  basePath: "/kanban",
+  assetPrefix: "/kanban-static",
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/kanban",
+    },
+    {
+      source: "/kanban/images/:path*",
+      destination:
+        process.env.NODE_ENV !== "production"
+          ? `/kanban/images/:path*`
+          : `https://${process.env.NEXT_PUBLIC_ROOT_URL}/kanban/images/:path*`,
+    },
+  ],
 };
 
 export default nextConfig;
